d0b16b63a24f8a65527286b4dd8c62556a778010
Emrecan Balgün <deathlygrave@gmail.com>
2022-07-16 02:39:20 +0300

    update folder structure and tailwind

--- introduction/README.md
+++
@@ -1,4 +0,0 @@
-# Installition
-
-* npx tailwindcss -o tailwind.css (create tailwind.css file)
-* npx tailwindcss init (create tailwind.config.js)
--- introduction/tailwind.config.js
+++ introduction/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
+  mode: 'jit',
+  content: ['./src/**/*.html'],
   theme: {
     extend: {},
   },
