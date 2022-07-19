4086fce015c242747f62a8edcc3b3c3de7db9ddd
Emrecan Balgün <deathlygrave@gmail.com>
2022-07-19 19:35:33 +0300

    add header section

--- introduction/tailwind.config.js
+++ introduction/tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   mode: 'jit',
   content: ['./src/**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        prototurk: '#322445a1',
+      },
+      spacing: {
+        '15': '3.75rem',
+      }
+    },
   },
   plugins: [],
 }
